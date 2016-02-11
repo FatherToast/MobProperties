288b4064a1887d0b96c2f586245bc891f9688033
FatherToast <father.toast@hotmail.com>
2016-02-11 16:40:42 -0600

    1.0.0

--- src/main/java/toast/mobProperties/_MobPropertiesMod.java
+++ src/main/java/toast/mobProperties/_MobPropertiesMod.java
@@ -27,7 +27,7 @@ public class _MobPropertiesMod {
     // This mod's id.
     public static final String MODID = "MobProperties";
     // This mod's version.
-    public static final String VERSION = "0.4.3";
+    public static final String VERSION = "1.0.0";
 
     // If true, this mod starts up in debug mode.
     public static final boolean debug = false;
--- src/main/java/toast/mobProperties/entry/PropertyGroupConditional.java
+++ src/main/java/toast/mobProperties/entry/PropertyGroupConditional.java
@@ -17,6 +17,7 @@ import net.minecraft.nbt.NBTTagString;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.EnumDifficulty;
+import net.minecraft.world.biome.BiomeGenBase;
 import toast.mobProperties.FileHelper;
 import toast.mobProperties.IPropertyReader;
 import toast.mobProperties._MobPropertiesMod;
@@ -59,9 +60,142 @@ public class PropertyGroupConditional extends PropertyGroup {
             }
             return invert;
         }
-        if (source != null && category.startsWith("killed_by_")) {
+        if (category.startsWith("damage_type_")) {
+            try {
+            	// Vanilla damage types: "inFire", "onFire", "lava", "inWall", "drown", "starve", "cactus", "fall", "outOfWorld",
+            	// "generic", "magic", "wither", "anvil", "fallingBlock", "mob", "player", "arrow", "fireball", "thrown",
+            	// "indirectMagic", "thorns", "explosion.player", "explosion"
+                return source.getDamageType() == category.substring(12) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.equals("killed_with_fire")) {
+            try {
+                return source.isFireDamage() != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.equals("killed_with_magic")) {
+            try {
+                return source.isMagicDamage() != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.equals("killed_with_projectile")) {
+            try {
+                return source.isProjectile() != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.equals("killed_with_indirect")) {
+            try {
+                return source.getEntity() != source.getSourceOfDamage() != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.equals("killed_with_explosion")) {
+            try {
+                return source.isExplosion() != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.equals("killed_with_unblockable")) {
+            try {
+                return source.isUnblockable() != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("killed_by_")) {
             try {
                 return ((Class) EntityList.stringToClassMapping.get(category.substring(10))).isAssignableFrom(source.getEntity().getClass()) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.equals("killer_on_ground")) {
+            try {
+            	return source.getEntity().onGround != invert;
+            }
+	        catch (Exception ex) {
+	            // Do nothing
+	        }
+	        return invert;
+    	}
+        if (category.equals("killer_burning")) {
+            try {
+            	return source.getEntity().isBurning() != invert;
+            }
+	        catch (Exception ex) {
+	            // Do nothing
+	        }
+	        return invert;
+    	}
+        if (category.equals("killer_wet")) {
+            try {
+            	return source.getEntity().isWet() != invert;
+            }
+	        catch (Exception ex) {
+	            // Do nothing
+	        }
+	        return invert;
+    	}
+        if (category.equals("killer_submerged")) {
+            try {
+            	return (source.getEntity().isInsideOfMaterial(Material.water) && !EnchantmentHelper.getAquaAffinityModifier((EntityLivingBase) source.getEntity())) != invert;
+            }
+	        catch (Exception ex) {
+	            // Do nothing
+	        }
+	        return invert;
+    	}
+        if (category.startsWith("killer_has_potion_")) {
+            try {
+                return ((EntityLivingBase) source.getEntity()).isPotionActive(FileHelper.readPotion(category.substring(18), entity.getClass().getName() + "\\killer", true)) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("check_killer_nbt_")) {
+            try {
+                String[] path = category.substring(17).split("/");
+                String[] data = PropertyGroupConditional.getOperatorData(path[path.length - 1]);
+                path[path.length - 1] = data[0];
+
+                return PropertyGroupConditional.compareNBT(source.getEntity(), path, data) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.equals("killer_wither_skeleton")) {
+            try {
+            	return ((EntitySkeleton) source.getEntity()).getSkeletonType() == 1 != invert;
             }
             catch (Exception ex) {
                 // Do nothing
@@ -166,15 +300,6 @@ public class PropertyGroupConditional extends PropertyGroup {
             }
             return invert;
         }
-        if (category.startsWith("in_biome_")) {
-            try {
-                return entity.worldObj.getBiomeGenForCoords((int) Math.floor(entity.posX), (int) Math.floor(entity.posZ)).biomeID == Integer.parseInt(category.substring(9)) != invert;
-            }
-            catch (Exception ex) {
-                // Do nothing
-            }
-            return invert;
-        }
         if (category.startsWith("on_block_")) {
             try {
                 Block block = FileHelper.readBlock(category.substring(9), entity.getClass().getName(), true);
@@ -195,6 +320,87 @@ public class PropertyGroupConditional extends PropertyGroup {
         if (category.startsWith("below_")) {
             try {
                 return (int) entity.posY < Integer.parseInt(category.substring(6)) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+
+        if (category.startsWith("in_biome_")) {
+            try {
+                return entity.worldObj.getBiomeGenForCoords((int) Math.floor(entity.posX), (int) Math.floor(entity.posZ)) == PropertyGroupConditional.getBiome(category.substring(9)) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("in_biome_type_")) {
+            try {
+                return entity.worldObj.getBiomeGenForCoords((int) Math.floor(entity.posX), (int) Math.floor(entity.posZ)).isEqualTo(PropertyGroupConditional.getBiomeType(category.substring(14))) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("biome_temp_")) {
+            try {
+                return entity.worldObj.getBiomeGenForCoords((int) Math.floor(entity.posX), (int) Math.floor(entity.posZ)).getTempCategory() == PropertyGroupConditional.getTempCategory(category.substring(11)) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("biome_height_below_")) {
+            try {
+                return entity.worldObj.getBiomeGenForCoords((int) Math.floor(entity.posX), (int) Math.floor(entity.posZ)).rootHeight < PropertyGroupConditional.getBiomeHeight(category.substring(19)) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("is_humid")) {
+            try {
+                return entity.worldObj.getBiomeGenForCoords((int) Math.floor(entity.posX), (int) Math.floor(entity.posZ)).isHighHumidity() != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("rainfall_above_")) {
+            try {
+            	// Rainfall benchmarks: 0.0 is no rain, 0.5 is default, >0.85 is considered humid (affects fire), 1.0 is the max set by vanilla
+                return entity.worldObj.getBiomeGenForCoords((int) Math.floor(entity.posX), (int) Math.floor(entity.posZ)).rainfall > Float.parseFloat(category.substring(15)) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("biome_temp_above_")) {
+            try {
+            	// Temperature benchmarks: -0.5 is the vanilla min, 0.2 is freezing point, 0.5 is default, 2.0 is the vanilla max
+            	// Temmperature ranges: (-0.5) cold (0.1) invalid/cold (0.2) medium (1.0) hot (2.0)
+                return entity.worldObj.getBiomeGenForCoords((int) Math.floor(entity.posX), (int) Math.floor(entity.posZ)).temperature > Float.parseFloat(category.substring(17)) != invert;
+            }
+            catch (Exception ex) {
+                // Do nothing
+            }
+            return invert;
+        }
+        if (category.startsWith("temp_above_")) {
+            try {
+            	// At or below 64 blocks, the actual temp is the same as the biome temp
+            	// For each block above 64, the biome temperature is reduced by 0.0016666...
+            	// Everywhere above 64 blocks, the height used to calculate temp reduction is increased by a random number of blocks
+                int x = (int) Math.floor(entity.posX);
+                int z = (int) Math.floor(entity.posZ);
+                return entity.worldObj.getBiomeGenForCoords(x, z).getFloatTemperature(x, (int) Math.floor(entity.posY), z) > Float.parseFloat(category.substring(11)) != invert;
             }
             catch (Exception ex) {
                 // Do nothing
@@ -286,21 +492,21 @@ public class PropertyGroupConditional extends PropertyGroup {
 
     // Returns the moon phase id from the given string.
     private static int getMoonPhaseId(String phase) {
-        if (phase.equalsIgnoreCase("FULL"))
+        if ("FULL".equalsIgnoreCase(phase))
             return 0;
-        if (phase.equalsIgnoreCase("WANING_GIBBOUS"))
+        if ("WANING_GIBBOUS".equalsIgnoreCase(phase))
             return 1;
-        if (phase.equalsIgnoreCase("THIRD_QUARTER") || phase.equalsIgnoreCase("WANING_HALF"))
+        if ("THIRD_QUARTER".equalsIgnoreCase(phase) || "WANING_HALF".equalsIgnoreCase(phase))
             return 2;
-        if (phase.equalsIgnoreCase("WANING_CRESCENT"))
+        if ("WANING_CRESCENT".equalsIgnoreCase(phase))
             return 3;
-        if (phase.equalsIgnoreCase("NEW"))
+        if ("NEW".equalsIgnoreCase(phase))
             return 4;
-        if (phase.equalsIgnoreCase("WAXING_CRESCENT"))
+        if ("WAXING_CRESCENT".equalsIgnoreCase(phase))
             return 5;
-        if (phase.equalsIgnoreCase("FIRST_QUARTER") || phase.equalsIgnoreCase("WAXING_HALF"))
+        if ("FIRST_QUARTER".equalsIgnoreCase(phase) || "WAXING_HALF".equalsIgnoreCase(phase))
             return 6;
-        if (phase.equalsIgnoreCase("WAXING_GIBBOUS"))
+        if ("WAXING_GIBBOUS".equalsIgnoreCase(phase))
             return 7;
         try {
             return Integer.parseInt(phase) % 8;
@@ -312,22 +518,125 @@ public class PropertyGroupConditional extends PropertyGroup {
 
     // Parses the world difficulty from a string.
     private static EnumDifficulty getDifficulty(String id) {
-        if (id.equalsIgnoreCase("PEACEFUL"))
+        if ("PEACEFUL".equalsIgnoreCase(id))
             return EnumDifficulty.PEACEFUL;
-        else if (id.equalsIgnoreCase("EASY"))
+        if ("EASY".equalsIgnoreCase(id))
             return EnumDifficulty.EASY;
-        else if (id.equalsIgnoreCase("NORMAL"))
+        if ("NORMAL".equalsIgnoreCase(id))
             return EnumDifficulty.NORMAL;
-        else if (id.equalsIgnoreCase("HARD"))
+        if ("HARD".equalsIgnoreCase(id))
             return EnumDifficulty.HARD;
-        else {
-            try {
-                return EnumDifficulty.getDifficultyEnum(Integer.parseInt(id));
-            }
-            catch (Exception ex) {
-                return null;
-            }
+        try {
+            return EnumDifficulty.getDifficultyEnum(Integer.parseInt(id));
         }
+        catch (Exception ex) {
+            return null;
+        }
+    }
+
+    // Parses a biome from a string.
+    private static BiomeGenBase getBiome(String id) {
+    	for (BiomeGenBase biome : BiomeGenBase.getBiomeGenArray()) {
+    		if (biome.biomeName.equals(id))
+				return biome;
+    	}
+        try {
+        	int biomeId = Integer.parseInt(id);
+        	_MobPropertiesMod.console("[WARNING]  Usage of numerical biome id! (" + id + "=\"" + BiomeGenBase.getBiomeGenArray()[biomeId].biomeName + "\") in conditional check!");
+            return BiomeGenBase.getBiomeGenArray()[biomeId];
+        }
+        catch (Exception ex) {
+            return null;
+        }
+    }
+
+    // Parses a biome from a string for use in the more relaxed biome check.
+    private static BiomeGenBase getBiomeType(String id) {
+    	if ("BEACH".equalsIgnoreCase(id))
+			return BiomeGenBase.beach;
+    	if ("DESERT".equalsIgnoreCase(id))
+			return BiomeGenBase.desert;
+    	if ("END".equalsIgnoreCase(id))
+			return BiomeGenBase.sky;
+    	if ("FOREST".equalsIgnoreCase(id))
+			return BiomeGenBase.forest;
+    	if ("HELL".equalsIgnoreCase(id))
+			return BiomeGenBase.hell;
+    	if ("HILLS".equalsIgnoreCase(id))
+			return BiomeGenBase.extremeHills;
+    	if ("JUNGLE".equalsIgnoreCase(id))
+			return BiomeGenBase.jungle;
+    	if ("MESA".equalsIgnoreCase(id))
+			return BiomeGenBase.mesa;
+    	if ("MUSHROOM".equalsIgnoreCase(id))
+			return BiomeGenBase.mushroomIsland;
+    	if ("OCEAN".equalsIgnoreCase(id))
+			return BiomeGenBase.ocean;
+    	if ("PLAINS".equalsIgnoreCase(id))
+			return BiomeGenBase.plains;
+    	if ("RIVER".equalsIgnoreCase(id))
+			return BiomeGenBase.river;
+    	if ("SAVANNA".equalsIgnoreCase(id))
+			return BiomeGenBase.savanna;
+    	if ("SNOW".equalsIgnoreCase(id))
+			return BiomeGenBase.icePlains;
+    	if ("STONE_BEACH".equalsIgnoreCase(id))
+			return BiomeGenBase.stoneBeach;
+    	if ("SWAMP".equalsIgnoreCase(id))
+			return BiomeGenBase.swampland;
+    	if ("TAIGA".equalsIgnoreCase(id))
+			return BiomeGenBase.taiga;
+    	return PropertyGroupConditional.getBiome(id);
+    }
+
+    // Parses the biome temperature category from a string.
+    private static BiomeGenBase.TempCategory getTempCategory(String temp) {
+    	if ("OCEAN".equalsIgnoreCase(temp))
+			return BiomeGenBase.TempCategory.OCEAN;
+		if ("COLD".equalsIgnoreCase(temp))
+			return BiomeGenBase.TempCategory.COLD;
+		if ("MEDIUM".equalsIgnoreCase(temp))
+			return BiomeGenBase.TempCategory.MEDIUM;
+		if ("WARM".equalsIgnoreCase(temp))
+			return BiomeGenBase.TempCategory.WARM;
+        return null;
+    }
+
+    // Parses a biome height level from a string.
+    private static float getBiomeHeight(String height) {
+        try {
+            return Integer.parseInt(height) / 32.0F - 2.0F;
+        }
+        catch (Exception ex) {
+        	// Do nothing
+        }
+    	if ("DEFAULT".equalsIgnoreCase(height))
+    		return 0.1F;
+    	if ("SHORES".equalsIgnoreCase(height))
+    		return 0.0F;
+    	if ("LOW_PLAINS".equalsIgnoreCase(height))
+    		return 0.125F;
+    	if ("MID_PLAINS".equalsIgnoreCase(height))
+    		return 0.2F;
+    	if ("LOW_HILLS".equalsIgnoreCase(height))
+    		return 0.45F;
+    	if ("MID_HILLS".equalsIgnoreCase(height))
+    		return 1.0F;
+    	if ("SHALLOW_WATERS".equalsIgnoreCase(height))
+    		return -0.5F;
+    	if ("OCEANS".equalsIgnoreCase(height))
+    		return -1.0F;
+    	if ("LOW_ISLANDS".equalsIgnoreCase(height))
+    		return 0.2F;
+    	if ("PARTLY_SUBMERGED".equalsIgnoreCase(height))
+    		return -0.2F;
+    	if ("ROCKY_WATERS".equalsIgnoreCase(height))
+    		return 0.1F;
+    	if ("HIGH_PLATEAUS".equalsIgnoreCase(height))
+    		return 1.5F;
+    	if ("DEEP_OCEANS".equalsIgnoreCase(height))
+    		return -1.8F;
+    	return Float.NaN;
     }
 
     // True if this should only execute when its category would not.
